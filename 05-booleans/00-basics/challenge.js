879aa4b9d3060848f0bdf9c296eefd33922aa2d5
JonEast87 <j.d.east@icloud.com>
2018-06-09 15:57:27 -0700

    done

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,22 @@
-module.exports.equalStrings = function(stringOne, stringTwo) {
-
+module.exports.inBetween = function (lower, middle, upper) {
+    return middle > lower && middle < upper ? true : false;
 };
 
-module.exports.notEqual = function(one, two) {
-
+module.exports.equalStrings = function (stringOne, stringTwo) {
+    return stringOne === stringTwo ? true : false;
 };
 
-module.exports.inBetween = function(lower, middle, upper) {
-
+module.exports.notEqual = function (one, two) {
+    return one === two ? false : true;
 };
 
-module.exports.outsideRanges = function(number) {
-
+module.exports.outsideRanges = function (number) {
+    return (number >= 10 && number <= 20) ? false :
+        number > 42 && number <= 75 ? false :
+        number > 1 && number < 6 ? false :
+        true;
 };
 
-module.exports.nameAndPrice = function(name, price) {
-
+module.exports.nameAndPrice = function (name, price) {
+    return name === 'LATimes' && price >= 1 || name === 'NYTimes' && price >= 1 ? true : false;
 };
