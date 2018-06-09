56b9b301e733753eca6dbcd58c7914d8ba0460c5
JonEast87 <j.d.east@icloud.com>
2018-06-09 13:51:56 -0700

    finished all but last problem on 04-strings

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,21 @@
-module.exports.formLetter = function(firstName, senderName, message) {
-
+module.exports.formLetter = function (firstName, senderName, message) {
+    return `Hello ${firstName},\n\n${message}\n\nSincerely,\n${senderName}`;
 };
 
-module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+module.exports.sliceItAndCombineIt = function (bigString, startA, endA, startB, endB) {
+    return bigString.substring(startA, endA) + bigString.substring(startB, endB);
 };
 
-module.exports.findFirstMatch = function(text, searchString) {
-
+module.exports.findFirstMatch = function (text, searchString) {
+    return text.search(searchString);
 };
 
-module.exports.findLastMatch = function(text, searchString) {
-
+module.exports.findLastMatch = function (text, searchString) {
+    return text.lastIndexOf(searchString);
 };
 
-module.exports.substringBetweenMatches = function(text, searchString) {
-
+module.exports.substringBetweenMatches = function (text, searchString) {
+    const beginning = text.search(searchString);
+    const ending = text.lastIndexOf(searchString);
+    return text.substring(beginning, ending);
 };
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Introduction
+
 This is the JavaScript exercise repo to accompany Bloc's Front-end Web Development apprenticeship.
 
 Follow the environment setup from the Roadmap before solving these exercises.
