4b34f8c5dc3fb6db664247300fd52503d91bf6f6
JonEast87 <j.d.east@icloud.com>
2018-06-10 19:45:03 -0700

    done

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,21 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function reversePlusOne(array) {
+    let reversedArray = array.slice().reverse();
+    reversedArray.unshift(1);
+    return reversedArray;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function plusesEverywhere(array) {
+    let add = '';
+    for (let i = 0; i < array.length; i++) {
+        if (i === array.length - 1) {
+            add += array[i];
+            return add;
+        }
+        add += array[i] + '+';
+    }
+    return add;
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function arrayQuantityPlusOne(array) {
+    return array.length + 1;
+};
