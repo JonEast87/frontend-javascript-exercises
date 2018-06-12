4acde59ae0bb36aa4788ca19cbd35f253fcedd7f
JonEast87 <j.d.east@icloud.com>
2018-06-11 21:49:29 -0700

    done with object loops

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function getKeys(obj) {
+    let array = new Array();
+    for (let key in obj) {
+        array.push(key);
+    }
+    return array;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function getValues(obj) {
+    let array = new Array();
+    for (let key in obj) {
+        array.push(obj[key]);
+    }
+    return array;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function objectToArray(obj) {
+    let array = new Array();
+    for (let key in obj) {
+        array.push(`${key} is ${obj[key]}`);
+    }
+    return array;
+};
