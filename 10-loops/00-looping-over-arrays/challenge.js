e819453453cb5a8b75fdf19bfa9b1434f29e4f52
JonEast87 <j.d.east@icloud.com>
2018-06-11 21:39:26 -0700

    done with array loops

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,13 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function sumNumbers(array) {
+    return array.length === 0 ? 0 : array.reduce((a, b) => a + b);
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function splitAndLowerCaseString(inputString) {
+    return inputString.toLowerCase().split(',');
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function addIndex(array) {
+    let stringArr = new Array();
+    array.forEach((element, index) => stringArr.push(`${index} is ${element}`));
+    return stringArr;
+};
