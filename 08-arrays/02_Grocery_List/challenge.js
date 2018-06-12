bf28bcf6c1ba52ea5b1adc0c085675d046d75b7b
JonEast87 <j.d.east@icloud.com>
2018-06-11 17:55:14 -0700

    finished with array modules

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,11 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function addItem(lastItem, array) {
+    if (array.indexOf(lastItem) === -1) {
+        array.push(lastItem);
+        return array;
+    }
+    return array;
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function reverseSortedList(array) {
+    return array.sort().reverse();
+};
