165c0080d186a31b8340f1dfa126bdd2e891635c
JonEast87 <j.d.east@icloud.com>
2018-06-12 18:34:04 -0700

    done

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,17 @@
-module.exports.stream = undefined;
+module.exports.stream = function stream(conditionalFN, actionFN) {
+    let timer = 5;
+    while (conditionalFN(timer)) {
+        actionFN();
+        timer--;
+    }
+};
 
-module.exports.sumNumbers = undefined;
-
+module.exports.sumNumbers = function sumNumbers(array) {
+    let sum = 0;
+    let i = 0;
+    while (i < array.length) {
+        sum += array[i];
+        i++;
+    }
+    return sum;
+};
